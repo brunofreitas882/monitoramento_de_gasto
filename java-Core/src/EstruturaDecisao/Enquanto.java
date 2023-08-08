f8bf647a1b8b9688f56e18f2c4ac626fe3035f89
tci-dev <tci-dev@192.168.66.176>
2023-08-07 21:56:53 -0300

    commit 07/08/23

--- java-Core/src/EstruturaDecisao/Enquanto.java
+++ java-Core/src/EstruturaDecisao/Enquanto.java
@@ -3,6 +3,18 @@ package EstruturaDecisao;
 public class Enquanto {
 
 	public static void main(String[] args) {
+		
+		int numeros = 1;
+		//while (numeros < 11) {
+			//numeros++;
+			//System.out.println(numeros);
+		//}
+		
+		while (numeros < 10){
+			System.out.println(numeros);
+			numeros++;
+			numeros++;
+		}
 	}
 
 }
