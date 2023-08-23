c89d782c4e6f4aa49dfe3b3b920c1e8a53b4668a
tci-dev <tci-dev@192.168.66.176>
2023-08-22 21:58:36 -0300

    comit!

---
+++ java-Core/src/aplication/Start_Conta.java
@@ -0,0 +1,32 @@
+package aplication;
+
+import java.util.Locale;
+
+import javax.swing.JOptionPane;
+
+import entities.Conta;
+
+public class Start_Conta {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		Conta conta;
+		int numeroConta = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o número da conta", "Abertura da conta", JOptionPane.PLAIN_MESSAGE));
+		String donoConta = JOptionPane.showInputDialog(null, "Informe o nome do dona da conta","Abertura de conta", JOptionPane.PLAIN_MESSAGE);
+		
+		String seraQueVaiTerDeposito = JOptionPane.showInputDialog(null, "Terá depósito", "Abertura da conta", JOptionPane.PLAIN_MESSAGE);
+		seraQueVaiTerDeposito = seraQueVaiTerDeposito.toUpperCase();
+		char temDEpositoSimNao = seraQueVaiTerDeposito.charAt(0);
+		
+		if(temDEpositoSimNao == 'Y') {
+		double depositoInicial	= Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o valor para depósito","Abertura de conta", JOptionPane.PLAIN_MESSAGE));
+		conta = new Conta (numeroConta, donoConta, depositoInicial);
+		} else {
+			conta = new Conta (numeroConta, donoConta);
+		}
+		//mostrar os dados da conta
+		JOptionPane.showMessageDialog(null, conta, "Dados na conta", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
---
+++ java-Core/src/entities/Conta.java
@@ -0,0 +1,54 @@
+package entities;
+
+public class Conta {
+	private int numeroConta;
+	private String donoConta;
+	private double saldoConta;
+	
+	//-------------------------------------------------------------------------------------
+	//Contrutores
+	public Conta(int numeroConta, String donoConta) {
+		this.numeroConta = numeroConta;
+		this.donoConta = donoConta;
+	}
+	
+	public Conta(int numeroConta, String donoConta, double saldoConta) {
+		this.numeroConta = numeroConta;
+		this.donoConta = donoConta;
+		this.saldoConta = saldoConta;
+	}
+	
+	//-------------------------------------------------------------------------------------
+	//métodos gets e sets
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+	public String getDonoConta() {
+		return donoConta;
+	}
+	public void setDonoConta(String donoConta) {
+		this.donoConta = donoConta;
+	}
+	public double getSaldoConta() {
+		return saldoConta;
+	}
+	//-------------------------------------------------------------------------------------
+	
+	//método depósito 
+	public void deposito(double valorDeposito) {
+		saldoConta += valorDeposito;
+	}
+	
+	//método de saque 
+	public void saque(double valorDeposito) {
+		saldoConta -= valorDeposito + 5.00;
+	}
+	
+	//método toString sobrecarregado.
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Conta: " + numeroConta + "\nproprietário: " + donoConta + String.format("\nSaldo da conta: %.2f", saldoConta);
+	}
+}
